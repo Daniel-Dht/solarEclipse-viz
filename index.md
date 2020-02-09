12806267b29bc18ea6934d8636d739d510994d14
Gregoire Gobert <gregoire.gobert@ecl17.ec-lyon.fr>
2020-02-09 11:18:44 +0100

    Premiere version site

--- index.md
+++ index.md
@@ -1,113 +1,133 @@
-<!DOCTYPE html>
+<!doctype html>
+<html lang="fr">
 <head>
   <meta charset="utf-8">
+  <title>Eclipses solaires - visualisation</title>
+  <link rel="stylesheet" href="style.css">
+  <link href="https://fonts.googleapis.com/css?family=Oswald|PT+Sans+Narrow&display=swap" rel="stylesheet">
   <script src="https://d3js.org/d3.v4.min.js"></script>
-  <script src="https://d3js.org/topojson.v2.min.js"></script>
-  <script src="https://d3js.org/d3-geo-projection.v1.min.js"></script>
-  
-  <link href="https://fonts.googleapis.com/css?family=Karla" rel='stylesheet' type='text/css'>
-    <style>
-    body { margin:0;position:fixed;top:0;right:0;bottom:0;left:0; }
-    svg { width:100%;height:100% }
-    path {fill:none;}
-    .graticule {
-      fill: none;
-      stroke: #777;
-      stroke-width: .5px;
-      stroke-opacity: .5;
-    }
-    .stroke {
-  	  fill: none;
-  		stroke: #000;
-  		stroke-width: 3px;
-		}
-    text {
-    	font-family: 'Karla' ;
-      font-weight : normal;
-    }
-  </style>
 </head>
 
+
 <body>
-  <script>
-    var width = 960;
-    var height = 500;
-    
-    var color = d3.scaleOrdinal(d3.schemeCategory20);
 
-    var graticule = d3.geoGraticule();
-    var svg = d3.select("body").append("svg");
-    var g = svg.append("g");
+	<!-- Panneau de gauche avec le logo, la légende et la liste des eclipses -->
+	<div class="left_panel">
+	
+		<!-- D'abord le logo-->
+		<div class="logo">LOGO ECLIPSE VIZ</div>
+		
+		<!-- Puis la légende des symboles pour les types d'éclipses -->
+		<div class="eclipse_type_legende">
+			<!-- Eclipse partielle-->
+			<div>
+				<span class="eclipse_type_legende_symbol">&#9680;</span><span>Partielle</span>
+			</div>
+			<!-- Eclipse annulaire-->
+			<div>
+				<span class="eclipse_type_legende_symbol">&#10687;</span><span>Annulaire</span>
+			</div>
+			<!-- Eclipse totale-->
+			<div>
+				<span class="eclipse_type_legende_symbol">&#11044;</span><span>Totale</span>
+			</div>
+			<!-- Eclipse hybride-->
+			<div>
+				<span class="eclipse_type_legende_symbol">&#9677;</span><span>Hybride</span>
+			</div>
+		</div>
+		
+		<!-- Le titre des variables bien alignees -->
+		<div class="eclipse_liste_item">
+			<div class="eclipse_liste_item_date"><i>Date</i></div>
+			<div class="eclipse_liste_item_heure"><i>Heure</i></div>
+			<div class="eclipse_liste_item_type"><i>Type</i></div>
+			<div class="eclipse_liste_item_param selector">
+				<!-- Choix du param -->
+				<span class="choosen_param"><i>Paramètre</i></span><span style="margin-left:30px;">&#9660;</span>
+				<div class="choose_param">
+					<div class="choose_param_ratio">Ratio</div>
+					<div class="choose_param_longueur">Longueur</div>
+					<div class="choose_param_duree">Durée</div>
+					<div class="choose_param_gamma">Gamma</div>
+				</div>
+				
+			</div>
+		</div>
+		
+		<!-- La liste des eclipses, les variables bien alignees -->
+		<div class="eclipse_liste_container">
+		
+			<!--
+			//Les items 
+			<div class="eclipse_liste_item">
+				<div class="eclipse_liste_item_date">01/01/2019</div>
+				<div class="eclipse_liste_item_heure">13:34</div>
+				<div class="eclipse_liste_item_type">&#11044;</div>
+				<div class="eclipse_liste_item_param">
+					//La barre et le nombre
+					<div class="barre">'</div>
+					<span>1287</span>
+				</div>
+			</div>
+			<div class="eclipse_liste_item">
+				<div class="eclipse_liste_item_date">03/05/2020</div>
+				<div class="eclipse_liste_item_heure">02:29</div>
+				<div class="eclipse_liste_item_type">&#10687;</div>
+				<div class="eclipse_liste_item_param">
+					// La barre et le nombre
+					<div class="barre" style="width:20%;">'</div>
+					<span>247</span>
+				</div>
+			</div>
+			-->
+			
+		</div>
+		
+	</div>
+	
+	
+	
+	<!-- Zone d'affchage à droite -->
+	<div class="display">
+		<!-- Bouton de switch entre Map et Saros -->
+		<div class="switch_container">
+			<!-- Symbole Map -->
+			<span style="color:rgb(0,31,63);">&#128506;</span>
+			<!-- switch bouton -->
+			<div class="switch">
+				<div class="switch_circle">-</div>
+			</div>
+			<!-- Symbole Saros -->
+			<span style="color:rgb(200,200,200);">&#10561;</span>
+		</div>
+		
+		<!-- La zone svg -->
+		<div class="svg_container">
+			<div class="axes_title">
+				<span>Date</span>
+				<span>Durée</span>
+				<span>Ratio</span>
+				<span>Longueur</span>
+			</div>
+			<svg></svg>
+		</div>
+		
+		<!-- Quelques infos supplémentaires -->
+		<div class="infos_supp">
+			<!-- Le titre -->
+			<div class="infos_supp_titre">Saros</div>
+			<!-- Les infos sur plusieurs colonnes -->
+			<div class="infos_supp_multiple_columns">
+				Survoler une éclipse solaire
+			</div>
+		</div>
+	</div>
+	
 
-    //https://bl.ocks.org/mbostock/3710082
-		var projection = d3.geoKavrayskiy7()
-    .scale(170)
-    .translate([width / 2, height / 2])
-    .precision(.1)
-    .rotate([-11,0]);
-    
-    var path = d3.geoPath().projection(projection);
 
-    svg.append("defs").append("path")
-    .datum({type: "Sphere"})
-    .attr("id", "sphere")
-    .attr("d", path);
-
-    svg.append("use")
-        .attr("class", "stroke")
-        .attr("xlink:href", "#sphere");
-
-    svg.append("path")
-        .datum(graticule)
-        .attr("class", "graticule")
-        .attr("d", path);
-
-    
-    var data = "https://piwodlaiwo.github.io/topojson//world-continents.json";
-    
-    d3.json(data, function(error, topology) {
-      var continents = topojson.feature(topology, topology.objects.continent).features;
-      
-      var centroids = continents.map(function (d){
-        return projection(d3.geoCentroid(d))
-  		});
-      
-      svg.selectAll(".continent")
-      .data(continents)
-      .enter()
-      .append("path")
-      .attr("d", path)
-      .attr("title", function(d,i) { 
-        return d.properties.continent;
-      })
-      .style("fill", function(d, i) { return color(i); });
-      
-       svg.selectAll(".name").data(centroids)
-    	 .enter().append("text")
-       .attr("x", function (d){ return d[0]; })
-       .attr("y", function (d){ return d[1]; })
- 		   .style("fill", "black")
-       .attr("text-anchor", "middle")
-       .text(function(d,i) {
-         return continents[i].properties.continent;
-       });
-
-     
-    })
-
-      //reference
-      /*
-      starting point: techslides.com/demos/d3/worldmap-template-d3v4.html
-      colors via neighbors: https://bl.ocks.org/mbostock/4180634
-      https://github.com/piwodlaiwo/topojson
-      http://bl.ocks.org/micahstubbs/8e15870eb432a21f0bc4d3d527b2d14f (look into using d3.tip.js)
-      
-      //Did not need polylabel.js for labels
-      https://bl.ocks.org/Fil/da021023d58b8ddefd165c65e37f796b
-      http://blockbuilder.org/pnavarrc/75ec1502f51f86c2f43e
-      http://bl.ocks.org/hugolpz/42955069888057aff8c2
-      http://blockbuilder.org/d3noob/401237468c9e38cea8c7
-      */
-    
-  </script>
+<script src="script.js"></script>
 </body>
+
+
+</html>
---
+++ script.js
@@ -0,0 +1,272 @@
+var svg=d3.select("svg");
+console.log(document.getElementsByTagName("svg")[0].getBBox());
+svg.attr("viewBox","0 0 100 100");
+svg.attr("preserveAspectRatio","none");
+var eclipse_liste_container=d3.select(".eclipse_liste_container");
+
+//Lecture du jeu de données
+var parseDate = d3.timeParse("%_Y %B %e");
+var parseTime = d3.timeParse("%H:%M:%S");
+var parseDuration = d3.timeParse("%Mm%Ss");
+var data = d3.csv('solar_1950_2050.csv', function(error, data){
+  
+  //parsage des données
+  data.forEach(function(d)
+  {
+		d['Catalog Number']=parseInt(d['Catalog Number']);
+		d['Calendar Date']=parseDate(d['Calendar Date']);
+		d['Eclipse Time']=parseTime(d['Eclipse Time']);
+		d['Delta T (s)']=parseInt(d['Delta T (s)']);
+		d['Lunation Number']=parseInt(d['Lunation Number']);
+		d['Saros Number']=parseInt(d['Saros Number']);
+		d['Gamma']=parseFloat(d['Gamma']);
+		d['Eclipse Magnitude']=parseFloat(d['Eclipse Magnitude']);
+		d['Sun Altitude']=parseInt(d['Sun Altitude']);
+		d['Sun Azimuth']=parseInt(d['Sun Azimuth']);
+		d['Path Width (km)']=parseInt(d['Path Width (km)']); 
+		d['Central Duration']=parseDuration(d['Central Duration']);
+		//Traitement des données manquantes : mises nulles
+		if(isNaN(d['Path Width (km)']))
+		{
+		  d['Path Width (km)']=parseInt("0");  
+		}
+		if((d['Central Duration'])===null)
+		{
+		  d['Central Duration']=parseDuration("0m0s");
+		}
+		
+		
+  });
+
+
+//AFFICHAGE LISTE DES ECLIPSES
+var formatDate = d3.timeFormat("%d/%m/%Y");
+var formatHeure = d3.timeFormat("%H:%M");
+var formatDuree = d3.timeFormat("%H:%M:%S");
+
+function show_saros(e)
+{
+	//Titre : Soros et son numéro
+	d3.select(".infos_supp_titre")
+	  .text("Saros "+ e['Saros Number'].toString());
+	  
+	//On repasse en 1 seule colomnes  
+	d3.select(".infos_supp_multiple_columns")
+	  .text("")
+	  .style("column-count","1");
+	
+	//Ajout de la ligende. Un .saros_item est une grid de 6 colomnes	
+	var saros_item = d3.select(".infos_supp_multiple_columns")
+						.append("div")
+						.attr("class","saros_item")
+						  
+	saros_item.append("span").html("<i>Date</i>");
+	saros_item.append("span").html("<i>Heure</i>");
+	saros_item.append("span").html("<i>Type</i>");
+	saros_item.append("span").html("<i>Durée</i>");
+	saros_item.append("span").html("<i>Ratio</i>");
+	saros_item.append("span").html("<i>Longueur (km)</i>");
+	  
+	
+	//Parcourt des polylines
+	d3.selectAll("polyline")
+	 .attr("stroke",function(d)
+				{
+					//Si meme saros
+					if(d["Saros Number"]===e["Saros Number"])
+					{
+						var color_selected = "rgb(0,31,63)";
+						
+						//Ajout des cercles sur les axes
+						svg.append("ellipse").attr("cx","8").attr("cy",date_scale(d['Calendar Date']).toString()).attr("rx","0.5").attr("ry","1.4").style("fill",color_selected);
+						svg.append("ellipse").attr("cx","36").attr("cy",duration_scale(d['Central Duration']).toString()).attr("rx","0.5").attr("ry","1.4").style("fill",color_selected);
+						svg.append("ellipse").attr("cx","64").attr("cy",magnitude_scale(d['Eclipse Magnitude']).toString()).attr("rx","0.5").attr("ry","1.4").style("fill",color_selected);
+						svg.append("ellipse").attr("cx","92").attr("cy",path_scale(d['Path Width (km)']).toString()).attr("rx","0.5").attr("ry","1.4").style("fill",color_selected);
+						
+						//Ajout d'un saros_item
+						var saros_item = d3.select(".infos_supp_multiple_columns")
+											.append("div")
+											.attr("class","saros_item")
+						 
+						//Remplissage de l'item
+						saros_item.append("span").text(formatDate(d['Calendar Date']));
+						saros_item.append("span").text(formatHeure(d['Eclipse Time']));
+						var eclipse_type;
+						if(d['Eclipse Type'].substring(0,1)=="P"){eclipse_type="&#9680;"}
+						if(d['Eclipse Type'].substring(0,1)=="A"){eclipse_type="&#10687;"}
+						if(d['Eclipse Type'].substring(0,1)=="T"){eclipse_type="&#x2B24;"}
+						if(d['Eclipse Type'].substring(0,1)=="H"){eclipse_type="&#9677;"}
+						saros_item.append("span").html(eclipse_type);
+						saros_item.append("span").text(formatDuree(d['Central Duration']).toString().substring(0,8));
+						saros_item.append("span").text(d['Eclipse Magnitude'].toString().substring(0,4));
+						saros_item.append("span").text(d['Path Width (km)'].toString());
+						
+						
+						return color_selected;
+						//return color(e["Saros Number"]);
+					}
+					//Sinon
+					else
+					{
+						return "rgba(230,230,230,0.1)";
+					}
+				})
+}
+
+function hide_saros()
+{
+	//On repasse au texte par defaut
+	d3.select(".infos_supp_titre")
+	  .text("Saros");
+	
+	//On repasse au texte par defaut 
+	d3.select(".infos_supp_multiple_columns")
+	  .text("Survoler une éclipse solaire");
+	  
+	
+	//On supprime les saros_items  
+	d3.selectAll(".saros_item").remove();
+	 
+	
+	//On supprime les cercles 
+	d3.selectAll("ellipse").remove();
+	
+	//On remet les polyline par defaut 
+	d3.selectAll("polyline").attr("stroke","rgb(180,180,180)")
+}
+
+
+//Creation des items
+var items = eclipse_liste_container.selectAll("div").data(data)
+						   .enter()
+						   .append("div")
+						   .attr("class","eclipse_liste_item")
+						   .on("mouseover",function(e){show_saros(e);})
+						   .on("mouseout",function(){hide_saros()})
+
+
+//Pour le formattage
+var formatDate = d3.timeFormat("%d/%m/%Y");
+var formatHeure = d3.timeFormat("%H:%M");
+var paramScale = d3.scaleLinear().domain(d3.extent(data,function(d){return d['Eclipse Magnitude']})).range([1,70]);
+//Changer de paramScale avec le choix du paramètre
+
+//Ajout de la date a chaque item
+items.append("div")
+	   .attr("class","eclipse_liste_item_date")
+	   .text(function(d){return formatDate(d['Calendar Date'])});
+
+//Ajout de l'heure a chaque item
+items.append("div")
+	   .attr("class","eclipse_liste_item_heure")
+	   .text(function(d){return formatHeure(d['Eclipse Time'])})
+
+//Ajout du type a chaque item
+items.append("div")
+	   .attr("class","eclipse_liste_item_type")
+	   .html(function(d)
+				{
+					if(d['Eclipse Type'].substring(0,1)=="P"){return "&#9680;"}
+					if(d['Eclipse Type'].substring(0,1)=="A"){return "&#10687;"}
+					if(d['Eclipse Type'].substring(0,1)=="T"){return "&#x2B24;"}
+					if(d['Eclipse Type'].substring(0,1)=="H"){return "&#9677;"}
+				})
+
+//Ajout du parametre a chaque item
+items.append("div")
+	   .attr("class","eclipse_liste_item_param")
+	   .html(function(d){return '<div class="barre" style="width:'+paramScale(d['Eclipse Magnitude']).toString().substring(0,4)+'%">.</div><span>'+d['Eclipse Magnitude'].toString().substring(0,4)+'</span>'})
+	   
+	   
+	   
+	   
+	   
+	   
+// AFFICHAGE DU GRAPHE DES SAROS
+
+//Les echelles
+var date_scale=d3.scaleTime()
+     				.domain(d3.extent(data, function(d) {return d['Calendar Date'];}))
+					.range([2,92]);
+					      
+    
+var duration_scale=d3.scaleTime()
+				.domain(d3.extent(data, function(d) {return d['Central Duration'];}))
+				.range([2,92]); 
+				
+				
+var magnitude_scale=d3.scaleLinear()
+				.domain(d3.extent(data, function(d) {return d['Eclipse Magnitude'];}))
+				.range([2,92]); 
+				
+				
+var path_scale=d3.scaleLinear()
+				.domain(d3.extent(data, function(d) {return d['Path Width (km)'];}))
+				.range([2,92]); 
+				
+
+//Affichage des traits				
+svg.selectAll("polyline")
+	 .data(data)
+	 .enter()
+	 .append("polyline")
+	 .attr("fill","none")
+	 .attr("stroke","rgb(180,180,180)")
+	 .attr("stroke-width","0.4")
+	 .on("mouseover",function(e){show_saros(e);})
+	 .on("mouseout",function(){hide_saros()})
+	 .attr("points",function(d){return "8,"+date_scale(d['Calendar Date']).toString()+" 36,"+duration_scale(d['Central Duration']).toString()+" 64,"+magnitude_scale(d['Eclipse Magnitude']).toString()+" 92,"+path_scale(d['Path Width (km)']).toString()});
+	 
+	 
+	 
+//Affichage des axes
+svg.append("line")
+   .attr("x1","8")
+   .attr("x2","8")
+   .attr("y1","2")
+   .attr("y2","98")
+   .attr("stroke","black")
+   .attr("stroke-width","0.4");
+svg.append("polygon")
+   .attr("points","7,95 8,100 9,95")
+   .attr("stroke","none")
+   .attr("fill","black");
+
+svg.append("line")
+   .attr("x1","36")
+   .attr("x2","36")
+   .attr("y1","2")
+   .attr("y2","98")
+   .attr("stroke","black")
+   .attr("stroke-width","0.4"); 
+svg.append("polygon")
+   .attr("points","35,95 36,100 37,95")
+   .attr("stroke","none")
+   .attr("fill","black");
+   
+svg.append("line")
+   .attr("x1","64")
+   .attr("x2","64")
+   .attr("y1","2")
+   .attr("y2","98")
+   .attr("stroke","black")
+   .attr("stroke-width","0.4");
+svg.append("polygon")
+   .attr("points","63,95 64,100 65,95")
+   .attr("stroke","none")
+   .attr("fill","black");
+   
+svg.append("line")
+   .attr("x1","92")
+   .attr("x2","92")
+   .attr("y1","2")
+   .attr("y2","98")
+   .attr("stroke","black")
+   .attr("stroke-width","0.4");
+svg.append("polygon")
+   .attr("points","91,95 92,100 93,95")
+   .attr("stroke","none")
+   .attr("fill","black");
+
+  
+});
